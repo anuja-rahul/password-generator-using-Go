f26350cd8f37a0683779a9edb90da7c48502a8e8
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-29 11:26:14 +0530

    minor changes

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func checkError(err error) {
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+const FILENAME string = "passwords.csv"
+
+func main() {
+	f, err := os.Open(FILENAME)
+	if err != nil {
+		f, err = os.Create(FILENAME)
+		checkError(err)
+		fmt.Println("Created FIle !")
+	}
+	fmt.Printf("file: %v\n", f)
 }
